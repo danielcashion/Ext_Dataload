008a852b747d269db9b919ba0ab240364cb54c74
Roman Zhukov <ruzhukov@gmail.com>
2020-03-20 15:18:27 -0400

    Enable CORS

--- src/lambda/src/add-job.py
+++ src/lambda/src/add-job.py
@@ -5,6 +5,10 @@ import traceback
 
 sqs = boto3.client('sqs')
 queue_url = 'https://sqs.us-east-1.amazonaws.com/564748484972/TMDataLoad'
+resHeaders = {'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+					'Access-Control-Allow-Methods': 'GET,POST,UPDATE,DELETE,OPTIONS',
+					'Access-Control-Allow-Origin': '*'
+}
 
 system_jobs_url = "https://api.tourneymaster.org/v2/system_jobs"
 
@@ -18,25 +22,27 @@ def lambda_handler(event, context):
 			'cognito:username': event['requestContext']['authorizer']['claims']['cognito:username']
 		}
 		res = add_job(message)
-		# TODO implement
 		message['job_id'] = res['MessageId']
 
 		requests.post(url=system_jobs_url,
 									data=json.dumps({'job_id': message['job_id'], 'status': 'Started'}),
 									headers={'Content-Type': 'application/json',
-																	'Authorization': 'Bearer {}'.format(message['token'])})
+														'Authorization':'Bearer {}'.format(message['token'])}					
+									)
 	except:
 		print(f'Error: {traceback.format_exc()}')
 		return {
 			'isBase64Encoded': False,
 			'statusCode': 404,
-			'body': json.dumps({ 'success': 'false', 'message': traceback.format_exc() })
+			'body': json.dumps({'success': 'false', 'message': traceback.format_exc()}),
+			'headers': resHeaders
 		}
 	else:
 		return {
 			'isBase64Encoded': False,
 			'statusCode': 200,
-			'body': json.dumps({ 'success': 'true', 'message': message })
+			'body': json.dumps({'success': 'true', 'message': message}),
+			'headers': resHeaders
 		}
 
 def add_job(message):
