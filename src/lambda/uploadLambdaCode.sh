7014995058b4a2a30d111558053f5e4fcb70cf02
rz <ruzhukov@gmail.com>
2020-12-10 10:01:17 -0500

    Update deployment

--- src/lambda/uploadLambdaCode.sh
+++ src/lambda/uploadLambdaCode.sh
@@ -5,10 +5,10 @@
 
 AWS_PROFILE=clubsports
 
-rm -R bin/*
+rm -Rf bin/*
 
 mkdir bin/add-job
-pip install --target ./bin/add-job requests
+pip3 install --target ./bin/add-job requests
 cp src/add-job.py bin/add-job
 pushd bin/add-job
 zip -r ../add-job-code.zip .
@@ -17,12 +17,12 @@ aws lambda update-function-code --function-name "TMDataLoadSTAGING-add-job" --zi
 aws lambda update-function-code --function-name "TMDataLoadPROD-add-job" --zip-file "fileb://bin/add-job-code.zip"
 
 mkdir bin/process-job
-pip install --target ./bin/process-job requests
+pip3 install --target ./bin/process-job requests
 cp src/process-job.py bin/process-job
 cp ../../tourneymachine_scraper/crawler.py bin/process-job
 pushd bin/process-job
 docker run -v $(pwd):/outputs -it lambci/lambda:build-python3.7 \
-pip install lxml -t /outputs/
+pip3 install lxml -t /outputs/
 chmod -R 755 .
 zip -r ../process-job-code.zip .
 popd
